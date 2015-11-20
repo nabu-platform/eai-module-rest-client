f889ce0ccd92404a035d8cd27cb29835a626ba51
alex <alex@nabu.be>
2015-11-20 09:25:53 +0100

    Updated

---
+++ src/main/java/be/nabu/module/rest/client/RESTClientGUIManager.java
@@ -0,0 +1,32 @@
+package be.nabu.module.rest.client;
+
+import java.io.IOException;
+import java.util.List;
+
+import be.nabu.eai.developer.MainController;
+import be.nabu.eai.developer.managers.base.BaseJAXBGUIManager;
+import be.nabu.eai.repository.resources.RepositoryEntry;
+import be.nabu.libs.property.api.Property;
+import be.nabu.libs.property.api.Value;
+
+public class RESTClientGUIManager extends BaseJAXBGUIManager<RESTClientConfiguration, RESTClientArtifact> {
+
+	public RESTClientGUIManager() {
+		super("REST Client", RESTClientArtifact.class, new RESTClientManager(), RESTClientConfiguration.class);
+	}
+
+	@Override
+	protected List<Property<?>> getCreateProperties() {
+		return null;
+	}
+
+	@Override
+	protected RESTClientArtifact newInstance(MainController controller, RepositoryEntry entry, Value<?>... values) throws IOException {
+		return new RESTClientArtifact(entry.getId(), entry.getContainer());
+	}
+
+	@Override
+	public String getCategory() {
+		return "Protocols";
+	}
+}
---
+++ src/main/java/be/nabu/module/rest/client/RESTClientManager.java
@@ -0,0 +1,18 @@
+package be.nabu.module.rest.client;
+
+import be.nabu.eai.repository.api.Repository;
+import be.nabu.eai.repository.managers.base.JAXBArtifactManager;
+import be.nabu.libs.resources.api.ResourceContainer;
+
+public class RESTClientManager extends JAXBArtifactManager<RESTClientConfiguration, RESTClientArtifact> {
+
+	public RESTClientManager() {
+		super(RESTClientArtifact.class);
+	}
+
+	@Override
+	protected RESTClientArtifact newInstance(String id, ResourceContainer<?> container, Repository repository) {
+		return new RESTClientArtifact(id, container);
+	}
+
+}
